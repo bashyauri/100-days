886c50758da44cb7af5a3e2d844e6fd26ac9211e
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-12-22 21:00:23 +0100

    learnt api

---
+++ kanye-api/Readme.md
@@ -0,0 +1,1 @@
+This is an project to test my skills in working with APIs
---
+++ kanye-api/main.py
@@ -0,0 +1,33 @@
+import os
+from tkinter import *
+import requests
+
+
+file_path = os.path.dirname(__file__)
+
+
+def get_quote():
+    url = "https://api.kanye.rest/"
+    response = requests.get(url=url)
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote)
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file=os.path.join(file_path, 'background.png'))
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=(
+    "Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file=os.path.join(file_path, 'kanye.png'))
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+window.mainloop()
