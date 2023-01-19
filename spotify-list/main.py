ab034281136af4ded7b3325311013d01d6095b10
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-01-19 04:00:28 +0100

    completed project

--- spotify-list/main.py
+++ spotify-list/main.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 year = input(
-    "Which year do you want to travel to? Type the date in the format YYYY-MM-DD:")
+    "Which year do you want to travel to? Type the date in the format YYYY-MM-DD:").split("-")[0]
 response = requests.get(f" https://www.billboard.com/charts/hot-100/{year}")
 chart_page = response.text
 
@@ -14,7 +14,6 @@ songs = [x.get_text().strip()
          for x in soup.find_all("h3", attrs={'class': 'a-no-trucate'})]
 clientid = "b3a11d94030647d5847fb140f7104db1"
 secret = "a06ea6649aed47e9a81ee801ddde72c7"
-uri = "http://example.com/callback/"
 
 
 spotty = spotipy.Spotify(
@@ -30,10 +29,19 @@ spotty = spotipy.Spotify(
 user_id = spotty.current_user()["id"]
 song_uri = []
 for song in songs:
-    result = spotty.search(q=f"track:{song} year:{year}", type="track")
+    result = spotty.search(
+        q=f"track:{song} year:{year}",  type="track", market="NG")
     print(result)
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uri.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
+print(song_uri)
+new_playlist = spotty.user_playlist_create(
+    user=user_id, name=f"{year} Billboard 100", public=False, description=f"{year} Billboard 100")
+playlist_id = new_playlist["id"]
+
+
+add_track = spotty.user_playlist_add_tracks(
+    user=user_id, playlist_id=playlist_id, tracks=song_uri)
